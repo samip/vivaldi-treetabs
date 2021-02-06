c2852b05d786352be8713a1379d73ea7a6854354
Sami Pellinen <sami.m.pellinen@gmail.com>
2021-02-06 11:25:11 +0200

    Finished VivaldiUIObserver

--- browserhook/observer.js
+++ browserhook/observer.js
@@ -28,11 +28,7 @@ class VivaldiUIObserver {
   }
 
   initTabContainerObserver(tabContainerParent) {
-    if (!tabContainerParent) {
-      console.error('Missing observe target')
-      return
-    }
-
+    console.log(`Init ${tabContainerParent}`)
     this.tabContainerObserver = new MutationObserver(this.findTabContainerFromMutations.bind(this))
     this.tabContainerObserver.observe(tabContainerParent, {
       attributes: false,
@@ -42,23 +38,26 @@ class VivaldiUIObserver {
   }
 
   initTabObserver(tabStripElement) {
-    if (!tabStripElement) {
-      console.error('Missing tabstrip element')
-      return
-    }
+    this.tabObserver = new MutationObserver(this.findTabsFromMutations.bind(this))
+    this.tabObserver.observe(tabStripElement, {
+      attributes: false,
+      childList: true,
+      characterData: false
+    })
   }
 
   onTabContainerCreated(tabContainerElement) {
-    console.log('added')
-    console.log(tabContainerElement)
-    // Refresh tree
+    console.log('added container')
 
     this.tabContainer.eventHandlers['onCreated'].forEach(eventHandler => eventHandler(tabContainerElement))
     // Start observing for tab-buttons
     const tabStrip = tabContainerElement.querySelector('.tab-strip')
-    console.log(tabStrip)
-    console.log(tabContainerElement)
-    this.initTabObserver(tabStrip)
+    // TODO: avoid searching whole document
+    waitForElement('.tab-strip', 5000)
+      .then(
+        element => this.initTabObserver(element),
+        error => console.error(error)
+      )
   }
 
   onTabContainerRemoved(tabContainerElement) {
@@ -69,9 +68,8 @@ class VivaldiUIObserver {
 
   onTabCreated(tabElement)  {
     const getTabId = node => {
-      const tabDiv = node.querySelector('.tab')
       // eg. <div class="tab" id="tab-15">
-      const idParts = tabDiv.id.split('-')
+      const idParts = node.id.split('-')
       return (idParts.length === 2) ? parseInt(idParts[1]) : null
     }
 
@@ -107,40 +105,47 @@ class VivaldiUIObserver {
       })
     })
   }
-
-  /*
-  Helpers
-  */
-
-  static tabstripParentSelector() { return '#main > .inner' }
 }
 
 
+function waitForElement(selector, rejectAfterMs) {
+  return new Promise((resolve, reject) => {
+    let element = document.querySelector(selector)
+    if (element) {
+      resolve(element)
+    }
+
+    const retryTime = 100
+    let totalPollTime = 0
+
+    var pollingTimer = setInterval(() => {
+      element = document.querySelector(selector)
+      if( element) {
+        resolve(element)
+      } else {
+        totalPollTime += retryTime
+        if (totalPollTime >= rejectAfterMs) {
+          reject(`${selector} did not resolve in ${rejectAfterMs} ms`)
+        }
+      }
+    }, retryTime)
+  });
+}
 
 
 const vivaldiUI = new VivaldiUIObserver()
 
-vivaldiUI.tabContainer.addCallback('onCreated', tabStripElement => console.log('refreshTree'))
-vivaldiUI.tabContainer.addCallback('onRemoved', tabStripElement => console.log('startObservering'))
-vivaldiUI.tabs.addCallback('onCreated', tabID => console.log('indent' + tabId))
+vivaldiUI.tabContainer.addCallback('onCreated', element => console.log('refreshTree'))
+vivaldiUI.tabContainer.addCallback('onRemoved', element => console.log('startObservering'))
+vivaldiUI.tabs.addCallback('onCreated', (element, tabId) => console.log('indent' + tabId))
 console.log(vivaldiUI)
 
 const retryTime = 100
 const maxRetryAttempts = 5
 var retryAttempts = 0
 
-var checkExist = setInterval(function() {
-  let tabStripParent = document.querySelector(VivaldiUIObserver.tabstripParentSelector())
-  if (tabStripParent) {
-    console.log("Exists!")
-    clearInterval(checkExist)
-    vivaldiUI.initTabContainerObserver(tabStripParent)
-   } else {
-     console.log("Does not exist")
-     retryAttempts += 1
-     if (retryAttempts >= maxRetryAttempts) {
-       clearInterval(checkExist)
-       console.log(`tabstripParentSelector was not found after ${retryAttempts} attempts`)
-     }
-   }
-}, retryTime)
+waitForElement('#main > .inner', 5000)
+  .then(
+    tabContainerParent => vivaldiUI.initTabContainerObserver(tabContainerParent),
+    error => console.error(error)
+  )
