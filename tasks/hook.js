533cd31264f600e4cb33a8107bb9609a808ab197
Sami Pellinen <sami.m.pellinen@gmail.com>
2021-02-13 08:32:22 +0200

    Browserhook concat task

---
+++ tasks/hook.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp')
+var concat = require('gulp-concat');
+
+gulp.task('hook', [], function() {
+  console.log("Concating browserhook to dist/treetabs.js");
+  gulp.src(["browserhook/!(Init)*.js" , "browserhook/Init.js"])
+    .pipe(concat('treetabs.js'))
+    .pipe(gulp.dest('dist/'));
+});
